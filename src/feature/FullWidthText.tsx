d2bb77c33d6b55cabee2a1ab10dbc059fece6075
Ron Courville <ronald.t.courville@kp.org>
2022-09-22 16:12:19 -0700

    Corrected output

--- src/feature/FullWidthText.tsx
+++ src/feature/FullWidthText.tsx
@@ -1,20 +1,17 @@
-import { ReactNode } from "react";
-
-
 const FullWidthText = () => (
   <div className="bg-gray-100">
     <div className="max-w-screen-lg mx-auto px-3 py-16">
       <div className="text-center text-2xl md:text-md xl:text-7xl tracking-tight mb-6">
-          The Pathways Study closely follows a diverse cohort of 4,505 women diagnosed
-          with invasive breast cancer between the years 2006 and 2013. With repeated
-          participant contacts, access to electronic health records, bio-specimen
-          collection and genetic array sequencing, this study has accumulated a trove
-          important information with the primary purpose of determining how lifestyle
-          and genetic factors impact the breast cancer survival experience.
-        </div>
+        The Pathways Study closely follows a diverse cohort of 4,505 women
+        diagnosed with invasive breast cancer between the years 2006 and 2013.
+        With repeated participant contacts, access to electronic health records,
+        bio-specimen collection and genetic array sequencing, this study has
+        accumulated a trove important information with the primary purpose of
+        determining how lifestyle and genetic factors impact the breast cancer
+        survival experience.
+      </div>
     </div>
   </div>
-  
 );
 
 export { FullWidthText };
